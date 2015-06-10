a27c23c437e58e50dc256a288a2aee436e5273e2
Arthur Barrett <abarrett@fas.harvard.edu>
2015-06-10 18:11:13 -0400

    Modified setup.py install_requires.

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         ],
 
         dependency_links=[
-            'http://github.com/Harvard-University-iCommons/django-auth-lti/archive/v1.2.0.zip#egg=django_auth_lti',
+                'http://github.com/Harvard-University-iCommons/django-auth-lti/tarball/master#egg=django_auth_lti-1.2.0',
         ],
 
         # List additional groups of dependencies here (e.g. development
