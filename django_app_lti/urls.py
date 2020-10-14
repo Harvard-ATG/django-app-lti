f22fa6d0c1b7e3bde95554f87af7254c2c381c41
Joshua Getega <jgetega@gmail.com>
2020-10-14 19:31:28 +0300

    Add app_name to url module

--- django_app_lti/urls.py
+++ django_app_lti/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import LTILaunchView, LTIToolConfigView, logout_view, logged_out_view
 
+app_name = 'lti'
 urlpatterns = [
     path('', LTILaunchView.as_view(), name='index'),
     path('launch', LTILaunchView.as_view(), name='launch'),
